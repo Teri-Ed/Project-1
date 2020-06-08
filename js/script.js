12cfe9717a201110bd243becddb54f30c3a21d4c
Teri Edwards <teri.edwards@outlook.com>
2020-06-08 10:35:29 -0700

    Working on carousel.

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ function getFigures() {
 function moveForward() {
     var pointer;
     var figures = getFigures();
-    for (var i = 0; i < figures.length; i++) {
+    for (var i = 0; i < figures.length; i+) {
         if (figures[i].className == 'visible') {
             figures[i].className = '';
             pointer = i;
         }
     }
-    if (++pointer == figures.length) {
+    if (+pointer == figures.length) {
         pointer = 0;
     }
     figures[pointer].className = 'visible';
